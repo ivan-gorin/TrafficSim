37484eadf4f4ef5d458e7d5a0c4ed434b4f89ec3
christopher171 <60505233+christopher171@users.noreply.github.com>
2020-01-31 14:59:27 +0300

    Add files via upload

---
+++ flow.h
@@ -0,0 +1,32 @@
+#include "vehicle.h"
+
+namespace CityFlow {
+    class Engine;
+    class Flow {
+        private:
+            VehicleInfo vehicleTemplate;
+            double interval;
+            double nowTime = 0;
+            double currentTime = 0;
+            int startTime = 0;
+            int endTime = -1;
+            int cnt = 0;
+            Engine *engine;
+            std::string id;
+        public:
+            Flow(const VehicleInfo &vehicleTemplate, double timeInterval,
+            Engine *engine, int startTime, int endTime, const std::string &id) 
+            : vehicleTemplate(vehicleTemplate), interval(timeInterval),
+              startTime(startTime), endTime(endTime), engine(engine), id(id) {
+                assert(timeInterval >= 1 || (startTime == endTime));
+                nowTime = interval;
+            }
+            void nextStep(double timeInterval);
+
+            std::string getId() const;
+
+            void reset();
+        }
+    };
+}
+
