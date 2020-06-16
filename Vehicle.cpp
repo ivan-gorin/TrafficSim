aceb611bbb66d22c2939c03b2c907ac5507faa9b
ivan-gorin <43433603+ivan-gorin@users.noreply.github.com>
2020-06-16 20:58:05 +0300

    Delete Vehicle.cpp

--- Vehicle.cpp
+++
@@ -1,9 +0,0 @@
-#include "Vehicle.h"
-#include <iostream>
-
-namespace traffic_model {
-    Vehicle::Vehicle(const Vehicle &vehicle) : vehicleinfo(vehicle.vehicleinfo), 
-        id(vehicle.id), engine(vehicle.engine) { }
-    Vehicle::Vehicle(const VehicleInfo &init, const std::string &id, Engine *engine) :
-        vehicleinfo(init), id(id), engine(engine) { }
-}
