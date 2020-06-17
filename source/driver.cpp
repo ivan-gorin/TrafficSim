1ad98e60938859530478c8211393ac27653eb74d
christopher171 <60505233+christopher171@users.noreply.github.com>
2020-06-17 14:46:56 +0300

    Add files via upload

---
+++ source/driver.cpp
@@ -0,0 +1,35 @@
+#include <driver.h>
+#include <iostream>
+#include <string>
+namespace CityFlow {
+    Driver::Driver(const std::string & type) {
+           if (type == "crazy") {
+               MyInfo.speedN = 0.75;
+               MyInfo.posAcN = 0.75;
+               MyInfo.negAcN = 0.75;
+               MyInfo.minGapN = 1;
+               MyInfo.turnSpeedN = 0.75;
+               MyInfo.maxSpeedN = 1;
+               MyInfo.usualNegAcN = 0.75;
+               MyInfo.usualPosAcN = 0.75;
+           } else if (type == "normal") {
+               MyInfo.speedN = 0.75;
+               MyInfo.posAcN = 0.75;
+               MyInfo.negAcN = 0.75;
+               MyInfo.minGapN = 1;
+               MyInfo.turnSpeedN = 0.75;
+               MyInfo.maxSpeedN = 1;
+               MyInfo.usualNegAcN = 0.75;
+               MyInfo.usualPosAcN = 0.75;
+           } else if (type == "slow") {
+               MyInfo.speedN = 0.5;
+               MyInfo.posAcN = 0.5;
+               MyInfo.negAcN = 0.5;
+               MyInfo.minGapN = 2;
+               MyInfo.turnSpeedN = 0.5;
+               MyInfo.maxSpeedN = 0.75;
+               MyInfo.usualNegAcN = 0.5;
+               MyInfo.usualPosAcN = 0.5;
+           }
+       }
+}
